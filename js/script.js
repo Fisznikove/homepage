f2300625b4be9aab5ba63c225d46acbf58fa8c1e
Kasia Szewczyk <fisznikove@gmail.com>
2023-01-24 15:48:31 +0100

    functionsadded

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-console.log("Cześć");
-
-let button = document.querySelector(".button");
-let changeColorButtonElement = document.querySelector(".button__changeColor");
-let body = document.querySelector(".body");
-
-
-
-button.addEventListener("click", () => {
-   body.classList.toggle("body--dark");
-   if (body.classList.contains("body--dark")) {
-      changeColorButtonElement.innerText = "jasne"
-   } else {
-      changeColorButtonElement.innerText = "ciemne"
+{
+   const welcome = () => {
+      console.log("Cześć");
    }
-})
+   const toggleBackground = () => {
+      const changeColorButtonElement = document.querySelector(".button__changeColor");
+      const body = document.querySelector(".body");
+      body.classList.toggle("body--dark");
+      if (body.classList.contains("body--dark")) {
+         changeColorButtonElement.innerText = "jasne"
+      } else {
+         changeColorButtonElement.innerText = "ciemne"
+      }
+   }
+
+   welcome();
+
+   const init = () => {
+      const button = document.querySelector(".button");
+      button.addEventListener("click", toggleBackground
+      );
+
+   }
+   init();
+
+
+
+
+
+
+}
